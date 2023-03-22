fd4f839143d792bcebe2c1caca9885143a351561
PiekarzM <127319430+PiekarzM@users.noreply.github.com>
2023-03-22 14:11:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    double a = 2;
+    double b = 8;
+    double sum = a+b;
+    double roz = a-b;
+    double iloczyn = a*b;
+    double iloraz = a/b;
+
+    System.out.println("Suma wynosi" + sum + "Różnica wynosi" + roz + "Iloczyn wynosi" + iloczyn + " Iloraz wynosi" + iloraz);
+    
+    
+  }
+}
